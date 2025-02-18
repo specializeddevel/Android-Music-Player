1ab426886e4f9c58f9c98fcf938c90f580b39b4d
Raúl Burgos Murray <specializeddevel@gmail.com>
2025-02-18 19:12:57 -0400

    Add NavigationView and temp menu

--- app/src/main/java/com/raulburgosmurray/musicplayer/PlaylistActivity.kt
+++ app/src/main/java/com/raulburgosmurray/musicplayer/PlaylistActivity.kt
@@ -17,7 +17,7 @@ class PlaylistActivity : AppCompatActivity() {
         binding = ActivityPlaylistBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.DrawerLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
