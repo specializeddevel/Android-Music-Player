2920c1946011c17ab051936b0df2b8aa71ab617b
Raul Burgos Murray <specializeddevel@gmail.com>
2025-02-25 19:51:17 -0400

    Add glide dependencies for manage images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -49,4 +50,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //Glide for image loading
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
