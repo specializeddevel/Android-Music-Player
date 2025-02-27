d0250fff8837abd5e8e7e9bc1a17c0b0924573ef
Raul Burgos Murray <specializeddevel@gmail.com>
2025-02-26 23:06:51 -0400

    Format song duration and lets play the first song
    Implemented a singleton design pattern (by lazy), this help to have a single instance of MediaPlayer

--- app/src/main/java/com/raulburgosmurray/musicplayer/Music.kt
+++ app/src/main/java/com/raulburgosmurray/musicplayer/Music.kt
@@ -1,5 +1,7 @@
 package com.raulburgosmurray.musicplayer
 
+import java.util.concurrent.TimeUnit
+
 data class Music(
     val id:String,
     val title: String,
@@ -9,3 +11,14 @@ data class Music(
     val path: String,
     val artUri: String
 )
+
+fun formatDuration(duration:Long):String{
+    val hours = TimeUnit.HOURS.convert(duration, TimeUnit.MILLISECONDS)
+    val minutes = TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS) % 60
+    val seconds = TimeUnit.SECONDS.convert(duration, TimeUnit.MILLISECONDS) % 60
+    if(hours>0) {
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    } else {
+        return String.format("%02d:%02d", minutes, seconds)
+    }
+}
