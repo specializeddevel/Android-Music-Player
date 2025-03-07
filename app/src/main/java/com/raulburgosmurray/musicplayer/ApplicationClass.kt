5b914718d475fbeab189d21288a3f2e7d1947f77
Raul Burgos Murray <specializeddevel@gmail.com>
2025-03-06 22:33:03 -0400

    Create ApplicationClass

---
+++ app/src/main/java/com/raulburgosmurray/musicplayer/ApplicationClass.kt
@@ -0,0 +1,26 @@
+package com.raulburgosmurray.musicplayer
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+
+class ApplicationClass: Application() {
+    companion object{
+        const val CHANNEL_ID = "channel1"
+        const val PLAY = "play"
+        const val NEXT = "next"
+        const val PREVIUS = "previus"
+        const val EXIT = "exit"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(CHANNEL_ID, "Now playin song", NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.description = "This is an important channel for showing song!"
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+}
